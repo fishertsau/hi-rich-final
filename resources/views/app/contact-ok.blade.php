ee93c0204f97301c38b4c38e5c344b8c7fc0a6a6
fisher_tsau <fisher_tsau@mail.chungyo.net>
2020-03-01 23:55:52 +0800

    update contact-ok page

--- resources/views/app/contact-ok.blade.php
+++ resources/views/app/contact-ok.blade.php
@@ -1,18 +1,20 @@
 @extends('app.layouts.master')
 
 @section('content')
-    <section class="contact-box">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="send-success">
-                        <div class="section-title">-聯絡訊息已送出-</div>
-                        <div class="contact-info-box">
-                            <p class="text-info">您的聯絡訊息已成功送出,我們將盡快與您聯絡!</p>
+    <div class="page-bg">
+        <section class="contact-box">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="send-success">
+                            <div class="section-title">-聯絡訊息已送出-</div>
+                            <div class="contact-info-box">
+                                <p class="text-info">您的聯絡訊息已成功送出,我們將盡快與您聯絡!</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 @endsection
