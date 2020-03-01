2e0e6f0917248519a436d7333d881386e294a546
fisher_tsau <fisher_tsau@mail.chungyo.net>
2020-03-01 23:52:37 +0800

    update contact-ok page

--- resources/views/app/contact-ok.blade.php
+++ resources/views/app/contact-ok.blade.php
@@ -4,19 +4,14 @@
     <section class="contact-box">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-4 col-12">
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <div class="section-title">-聯絡訊息已送出-</div>
-                    <div class="contact-info-box">
-                        <div>
-                            <p>您的聯絡訊息已成功送出,我們將盡快與您聯絡!</p>
+                <div class="col-12">
+                    <div class="send-success">
+                        <div class="section-title">-聯絡訊息已送出-</div>
+                        <div class="contact-info-box">
+                            <p class="text-info">您的聯絡訊息已成功送出,我們將盡快與您聯絡!</p>
                         </div>
                     </div>
                 </div>
-                <hr>
             </div>
         </div>
     </section>
